ca343fc7d9f3fc1aebe369caf9817f6d7fc12efc
Rahul Patel <400rahulpatel@gmail.com>
2025-07-14 07:29:12 +0530

    adding condition to check availability of city in database

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -12,12 +12,12 @@ const Card=()=>{
     
     return(
         <div>
-        
+        {weatherdata.data.location ? (
         <div className="weather">
             <img src={weatherdata?.data?.current?.condition.icon} />
         <h4>{weatherdata?.data?.current?.temp_c} C</h4>
         <p>{weatherdata?.data?.location?.name},{weatherdata?.data?.location?.region},{weatherdata?.data?.location?.country}</p>
-        </div>
+        </div>): (<> Location not available</>)}
         </div>
     )
 
